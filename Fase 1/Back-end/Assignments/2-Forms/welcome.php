65ef3b904642e8d8c9c726473889ce681e21bb4e
Thomas Koman <99066789@mydavinci.nl>
2023-03-23 14:34:56 +0100

    no more funny rickrolls

--- Fase 1/Back-end/Assignments/2-Forms/welcome.php
+++ Fase 1/Back-end/Assignments/2-Forms/welcome.php
@@ -1,8 +1,36 @@
+<?php
+if($_POST != [])
+{
+    function clean_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    ?>
+    Welcome <?php echo clean_input($_POST["name"]); ?><br>
+    Your email address is: <?php echo clean_input($_POST["mail"]); ?>
+    <?php
+}
+else{?>
+<!DOCTYPE html>
 <html>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <main>
+            <form method="post" id="inputFields" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <label>Name:</label>
+                    <input type="text" name="name" id="name"><br><br>  
+                    <label>E-Mail</label>
+                    <input type="text" name="mail" id="mail"><br>
+                    <button>Submit</button>
+            </form>
+        </main>
+    </body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["mail"]; ?>
-
-</body>
 </html>
+<?php } ?>
