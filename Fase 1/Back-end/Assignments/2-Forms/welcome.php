c0883d4ec84db67fa0e6e7860a6291d37f42cbf8
Thomas Koman <99066789@mydavinci.nl>
2023-03-28 11:10:24 +0200

    changed the code to be in one file and also made it more D R Y

--- Fase 1/Back-end/Assignments/2-Forms/welcome.php
+++ Fase 1/Back-end/Assignments/2-Forms/welcome.php
@@ -1,14 +1,33 @@
-<?php
-$nameErr = $mailErr = "*";
-if ((empty($_POST["name"]) || !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST["mail"]) || empty($_POST["mail"]))) {
-    if (empty($_POST["name"])) {
-        $nameErr = "*Input is required";
+<?php    
+    $nameErr = $mailErr = "*";
+    $errors = [];
+    $fields = ["name", "mail"];
+    function clean_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-    if (!preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST["mail"])) {
-        $mailErr = "*Mail is invalid";
+
+if (empty($_POST["name"])|| empty($_POST["mail"])  || !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST["mail"])){
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        foreach ($_POST as $key => $value) {
+            if($key == "mail"){
+                if (!preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $key)) {
+                    $errors[$key] = "*Mail is invalid";
+                }
+            }
+            if(empty($value)){
+                $errors[$key] = "*Input is required";
+            }
+        }
     }
-    if (empty($_POST["mail"])) {
-        $mailErr = "*Input is required";
+    else{
+        foreach ($fields as $key ) {
+            $errors[$key] = "*";
+        }
     }
     ?>
     <!DOCTYPE html>
@@ -21,15 +40,15 @@ if ((empty($_POST["name"]) || !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/",
 
     <body>
         <main>
-            <form method="post" id="inputFields" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <label>Name:</label>
-                <input type="text" name="name" id="name" value="<?php echo $_POST["name"];?>">
-                <span class="error"><?php echo $nameErr ?></span>
-                <br><br>
-                <label>E-Mail</label>
-                <input type="text" name="mail" id="mail" value="<?php echo $_POST["mail"];?>">
-                <span class="error"><?php echo $mailErr ?></span>
-                <br>
+            <form method="post" id="inputFields" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+                <?php 
+                    foreach ($fields as $key) {?>
+                        <label><?php $keyCap = strtoupper($key[0]) . substr($key, 1); echo $keyCap?>:</label>
+                        <input type="text" name=<?php echo $key?> id=<?php echo $key?> value="<?php if($_SERVER["REQUEST_METHOD"] == "POST"){echo clean_input($_POST[$key]);}?>">
+                        <span class="error"><?php echo $errors[$key]?></span>
+                        <br><br>
+                    <?php }
+                ?>
                 <button>Submit</button>
             </form>
         </main>
@@ -37,17 +56,10 @@ if ((empty($_POST["name"]) || !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/",
 
     </html>
     <?php
-} else if ($_POST != [] && (!$_POST["name"]) || !empty("mail")) {
-    function clean_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
+} 
+else{
     ?>
-        Welcome
+    Welcome
     <?php echo clean_input($_POST["name"]); ?><br>
         Your email address is:
     <?php echo clean_input($_POST["mail"]); ?>
