944ab34268a72980f3763aab3f636b6fcb30d4c7
Thomas Koman <tomsomgamer999@gmail.com>
2023-04-03 12:47:33 +0200

    already used require in madlibs, but alright ill play along

---
+++ Bovenbouw/Fase 1/Back-end/Assignments/3-Templating/index.php
@@ -0,0 +1,16 @@
+<?php
+    require("variables.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./Styling/style.css">
+</head>
+
+<body>
+    <p><?=$num?></p>
+</body>
+
+</html>
