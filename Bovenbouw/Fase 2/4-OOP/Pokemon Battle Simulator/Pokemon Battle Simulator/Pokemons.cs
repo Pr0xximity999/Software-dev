6f87e71c38df8a9290f475c16e26e802bf516fe0
Thomas Koman <99066789@mydavinci.nl>
2023-04-17 21:10:40 +0200

    Started with c# (letsgo)

---
+++ Bovenbouw/Fase 2/4-OOP/Pokemon Battle Simulator/Pokemon Battle Simulator/Pokemons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pokemons
+{
+
+    class Pokemon
+    {
+        private string name;
+        private string strength;
+        private string weakness;
+
+        public Pokemon(string name, string strength, string weakness)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.weakness = weakness; 
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void Battlecry(string cry)
+        {
+            Console.WriteLine($"{cry}!");
+        }
+    }
+}
---
+++ Bovenbouw/Fase 2/4-OOP/Pokemon Battle Simulator/Pokemon Battle Simulator/program.cs
@@ -0,0 +1,42 @@
+﻿using Pokemons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Battle_Simulator
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+            Thread.Sleep(1000);
+            Console.WriteLine("You have a new charmender!");
+            Thread.Sleep(1000);
+            while (true)
+            {
+                Console.WriteLine("What do you want to name him/her/they/them/xim/xer/attack chopper/ze/zer?");
+                string name = Console.ReadLine();
+                if (name == null) { name = ""; }
+
+                if(name != "")
+                {
+                    Pokemon charmender = new Pokemon(name, "fire", "water");
+                    for (int i = 0; i < 11; i++)
+                    {
+                        charmender.Battlecry(name);
+                        Thread.Sleep(100);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid name");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
